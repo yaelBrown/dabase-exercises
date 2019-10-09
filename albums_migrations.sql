b4966143533d22836a745c1dc9700a8c7591128d
yaelBrown <yaelrbrown@gmail.com>
2019-10-09 16:00:00 -0500

    Finished exercise

---
+++ albums_migrations.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id int UNSIGNED AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY(id),
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date INT(4),
+    sales FLOAT(10,2),
+    genre VARCHAR(100)
+);
